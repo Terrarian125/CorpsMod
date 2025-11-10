9ad41f2ab52e2f63fe6f3697d71cc220b0453dab
正人 髙橋 <240169@jc-21.jp>
2025-11-10 13:44:49 +0900

    Kiltの剣と行商システム

---
+++ Common/Systems/ExampleTravelingMerchantSystem.cs
@@ -0,0 +1,72 @@
+﻿using CorpsMod.Content.NPCs;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace CorpsMod.Common.Systems
+{
+	// ExampleTravelingMerchantSystem の代わりに TravelingMerchantSystem の名前を使用します
+	public class TravelingMerchantSystem : ModSystem
+	{
+		// クエスト完了による定住フラグ。ワールドに保存され、定住NPCスポーンの条件となります。
+		public static bool HasSettled = false;
+
+		// ExampleTravelingMerchant.UpdateTravelingMerchant のロジックはこのシステムで呼び出されます。
+		public override void PreUpdateWorld() {
+			// 定住していない場合にのみ、行商人としての出現ロジックを実行します。
+			if (!HasSettled) {
+				ExampleTravelingMerchant.UpdateTravelingMerchantLogic();
+			}
+		}
+
+		public override void SaveWorldData(TagCompound tag) {
+			tag["HasSettled"] = HasSettled; // 定住フラグを保存
+
+			// 定住後は行商人としてのデータは不要ですが、互換性のため残しておきます。
+			tag["shopItems"] = ExampleTravelingMerchant.shopItems;
+			if (ExampleTravelingMerchant.spawnTime != double.MaxValue) {
+				tag["spawnTime"] = ExampleTravelingMerchant.spawnTime;
+			}
+		}
+
+		public override void LoadWorldData(TagCompound tag) {
+			HasSettled = tag.GetBool("HasSettled"); // 定住フラグをロード
+
+			ExampleTravelingMerchant.shopItems.Clear();
+			ExampleTravelingMerchant.shopItems.AddRange(tag.Get<List<Item>>("shopItems"));
+			if (!tag.TryGet("spawnTime", out ExampleTravelingMerchant.spawnTime)) {
+				ExampleTravelingMerchant.spawnTime = double.MaxValue;
+			}
+		}
+
+		public override void ClearWorld() {
+			HasSettled = false; // 定住フラグをリセット
+
+			ExampleTravelingMerchant.shopItems.Clear();
+			ExampleTravelingMerchant.spawnTime = double.MaxValue;
+		}
+
+		public override void NetSend(BinaryWriter writer) {
+			// 定住フラグを同期
+			writer.Write(HasSettled);
+
+			// ショップアイテムを同期
+			writer.Write(ExampleTravelingMerchant.shopItems.Count);
+			foreach (Item item in ExampleTravelingMerchant.shopItems) {
+				ItemIO.Send(item, writer, writeStack: true);
+			}
+		}
+
+		public override void NetReceive(BinaryReader reader) {
+			HasSettled = reader.ReadBoolean(); // 定住フラグを受信
+
+			ExampleTravelingMerchant.shopItems.Clear();
+			int count = reader.ReadInt32();
+			for (int i = 0; i < count; i++) {
+				ExampleTravelingMerchant.shopItems.Add(ItemIO.Receive(reader, readStack: true));
+			}
+		}
+	}
+}
--- Common/Systems/TravelingMerchantSystem.cs
+++ Common/Systems/TravelingMerchantSystem.cs
@@ -1,55 +1,72 @@
-using CorpsMod.Content.NPCs;
-using System.Collections.Generic;
-using System.IO;
-using Terraria;
-using Terraria.ModLoader;
-using Terraria.ModLoader.IO;
+using CorpsMod.Content.NPCs; // CorpsMod.Content.NPCs を使用
+using System.Collections.Generic; // System.Collections.Generic を使用
+using System.IO; // System.IO を使用
+using Terraria; // Terraria を使用
+using Terraria.ModLoader; // Terraria.ModLoader を使用
+using Terraria.ModLoader.IO; // Terraria.ModLoader.IO を使用
 
 namespace CorpsMod.Common.Systems
 {
 	public class TravelingMerchantSystem : ModSystem
 	{
+		// ワールドの更新（ゲーム内時間進行）前に呼び出されます
 		public override void PreUpdateWorld() {
+			// 行商NPCの出現/デスポーンロジックを更新
 			ExampleTravelingMerchant.UpdateTravelingMerchant();
 		}
 
+		// ワールドデータ保存時に呼び出されます
 		public override void SaveWorldData(TagCompound tag) {
+			// ショップアイテムのリストを保存
 			tag["shopItems"] = ExampleTravelingMerchant.shopItems;
+			// スポーン時間がdouble.MaxValueでない（つまり、その日のスポーンが予定されている）場合にのみ保存
 			if (ExampleTravelingMerchant.spawnTime != double.MaxValue) {
 				tag["spawnTime"] = ExampleTravelingMerchant.spawnTime;
 			}
 		}
 
+		// ワールドデータロード時に呼び出されます
 		public override void LoadWorldData(TagCompound tag) {
+			// ショップアイテムリストをクリアし、ロードされたデータで埋めます
 			ExampleTravelingMerchant.shopItems.Clear();
 			ExampleTravelingMerchant.shopItems.AddRange(tag.Get<List<Item>>("shopItems"));
+
+			// スポーン時間をロード。データが存在しない場合（TryGetが失敗した場合）はdouble.MaxValueに設定します
 			if (!tag.TryGet("spawnTime", out ExampleTravelingMerchant.spawnTime)) {
 				ExampleTravelingMerchant.spawnTime = double.MaxValue;
 			}
 		}
 
+		// ワールドデータクリア時（新規ワールド作成時など）に呼び出されます
 		public override void ClearWorld() {
+			// ショップアイテムリストとスポーン時間をリセット
 			ExampleTravelingMerchant.shopItems.Clear();
 			ExampleTravelingMerchant.spawnTime = double.MaxValue;
 		}
 
+		// マルチプレイヤーでのデータ送信時に呼び出されます (サーバー -> クライアント)
 		public override void NetSend(BinaryWriter writer) {
-			// Note that NetSend is called whenever WorldData packet is sent.
-			// We use this so that shop items can easily be synced to joining players
-			// We recommend modders avoid sending WorldData too often, or filling it with too much data, lest too much bandwidth be consumed sending redundant data repeatedly
-			// Consider sending a custom packet instead of WorldData if you have a significant amount of data to synchronise
+			// 注: NetSendはWorldDataパケットが送信されるたびに呼び出されます。
+			// これを利用して、参加中のプレイヤーにショップアイテムを簡単に同期できるようにしています。
+			// Mod開発者には、冗長なデータを繰り返し送信することで帯域幅を消費しすぎないよう、
+			// WorldDataを頻繁に送信したり、過度に多くのデータで埋めたりしないことを推奨します。
+			// 同期するデータ量が非常に多い場合は、WorldDataの代わりにカスタムパケットの送信を検討してください。
 
+			// アイテムリストの個数を書き込み
 			writer.Write(ExampleTravelingMerchant.shopItems.Count);
+			// 各アイテムのデータを書き込み
 			foreach (Item item in ExampleTravelingMerchant.shopItems) {
-				ItemIO.Send(item, writer, writeStack: true);
+				ItemIO.Send(item, writer, writeStack: true); // スタック数も書き込む
 			}
 		}
 
+		// マルチプレイヤーでのデータ受信時に呼び出されます (サーバー <- クライアント、またはサーバーから全クライアント)
 		public override void NetReceive(BinaryReader reader) {
 			ExampleTravelingMerchant.shopItems.Clear();
-			int count = reader.ReadInt32();
+			int count = reader.ReadInt32(); // アイテムの個数を読み込み
 			for (int i = 0; i < count; i++) {
-				ExampleTravelingMerchant.shopItems.Add(ItemIO.Receive(reader, readStack: true));
+				// 各アイテムのデータを読み込み、リストに追加
+				ExampleTravelingMerchant.shopItems.Add(ItemIO.Receive(reader, readStack: true)); // スタック数も読み込む
 			}
 		}
 	}
