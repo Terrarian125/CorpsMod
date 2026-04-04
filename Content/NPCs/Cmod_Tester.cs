a67584d293d2ad54e1cb55ce00776c2e32388060
正人 髙橋 <240169@jc-21.jp>
2026-04-05 00:01:20 +0900

    ボスは出せたので高低差への対応を

--- Content/NPCs/Cmod_Tester.cs
+++ Content/NPCs/Cmod_Tester.cs
@@ -30,82 +30,86 @@ using Terraria.Localization;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 using Terraria.Utilities;
-
+using CorpsMod.Content.NPCs;
 
 namespace CorpsMod.Content.NPCs
 {
-	[AutoloadHead]
-	public class Cmod_Tester : ModNPC
-	{
-		public const string ShopName = "Shop";
+    [AutoloadHead]
+    public class Cmod_Tester : ModNPC
+    {
+        public const string ShopName = "Shop";
 
-		private enum TalkState
-		{
-			Normal,      // 通常会話
-			Challenge    // 挑戦モード
-		}
+        private enum TalkState
+        {
+            Normal,      // 通常会話
+            Challenge    // 挑戦モード
+        }
 
-		private TalkState state = TalkState.Normal;
+        private TalkState state = TalkState.Normal;
 
-		public override void SetStaticDefaults() {
-			Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.Guide];
-		}
+        public override void SetStaticDefaults() {
+            Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.Guide];
+        }
 
-		public override void SetDefaults() {
-			NPC.townNPC = true;
-			NPC.friendly = true;
-			NPC.width = 18;
-			NPC.height = 40;
-			NPC.aiStyle = 7;
-			NPC.damage = 10;
-			NPC.defense = 15;
-			NPC.lifeMax = 250;
-			NPC.HitSound = SoundID.NPCHit1;
-			NPC.DeathSound = SoundID.NPCDeath1;
-			NPC.knockBackResist = 0.5f;
-			AnimationType = NPCID.Guide;
-		}
+        public override void SetDefaults() {
+            NPC.townNPC = true;
+            NPC.friendly = true;
+            NPC.width = 18;
+            NPC.height = 40;
+            NPC.aiStyle = 7;
+            NPC.damage = 10;
+            NPC.defense = 15;
+            NPC.lifeMax = 250;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.knockBackResist = 0.5f;
+            AnimationType = NPCID.Guide;
+        }
 
-		public override List<string> SetNPCNameList() =>
-			new() { "Cmodガイド", "テスター", "CCテスター" };
+        public override List<string> SetNPCNameList() =>
+            new() { "Cmodガイド", "テスター", "CCテスター" };
 
-		public override string GetChat() {
-			return state switch {
-				TalkState.Challenge => "挑戦したい相手を選べ！",
-				_ => GetGuideLikeHint()
-			};
-		}
+        public override string GetChat() {
+            return state switch {
+                TalkState.Challenge => "挑戦したい相手を選べ！",
+                _ => GetGuideLikeHint()
+            };
+        }
 
-		public override void SetChatButtons(ref string button, ref string button2) {
-			switch (state) {
-				case TalkState.Normal:
-					button = "ショップ";
-					button2 = "挑戦";
-					break;
+        public override void SetChatButtons(ref string button, ref string button2) {
+            switch (state) {
+                case TalkState.Normal:
+                    button = "ショップ";
+                    button2 = "挑戦";
+                    break;
+                case TalkState.Challenge:
+                    button = "TestBoss"; // ボタン名を分かりやすく変更
+                    button2 = "戻る";
+                    break;
+            }
+        }
+
+        public override void OnChatButtonClicked(bool firstButton, ref string shop) {
+            Player player = Main.LocalPlayer;
+
+            switch (state) {
+                case TalkState.Normal:
+                    if (firstButton) {
+                        shop = ShopName;
+                    }
+                    else {
+                        state = TalkState.Challenge;
+                        Main.npcChatText = "挑戦するのか？";
+                    }
+                    break;
+
 				case TalkState.Challenge:
-					button = "キングスライム";
-					button2 = "戻る";
-					break;
-			}
-		}
-
-		public override void OnChatButtonClicked(bool firstButton, ref string shop) {
-			Player player = Main.LocalPlayer;
-
-			switch (state) {
-				case TalkState.Normal:
 					if (firstButton) {
-						shop = ShopName; // ショップ
-					}
-					else {
-						state = TalkState.Challenge;
-						Main.npcChatText = "挑戦するのか？";
-					}
-					break;
+						// 【ここを修正！】
+						// 間違い：SummonBoss(player, NPCID.TestBoss, "TestBoss");
+						// 正解：
+						//SummonBoss(player, ModContent.NPCType<TestBoss>(), "TestBoss");
 
-				case TalkState.Challenge:
-					if (firstButton) {
-						SummonBoss(player, NPCID.TestBoss, "TestBoss");
 						state = TalkState.Normal;
 					}
 					else {
@@ -114,94 +118,79 @@ namespace CorpsMod.Content.NPCs
 					}
 					break;
 			}
-		}
+        }
 
-		private string GetGuideLikeHint() {
-			if (Main.bloodMoon)
-				return "血だまりで釣りをしたら何が釣れるんだろうね";
-			if (!Main.dayTime)
-				return "夜だ、君は寝ないのかい？";
-			if (Main.dayTime && Main.eclipse)
-				return "日食の日は強敵が現れる…稼ぎ時でもある";
-			return "やあ、何か買うかい？それとも売る？";
-		}
+        private string GetGuideLikeHint() {
+            if (Main.bloodMoon)
+                return "血だまりで釣りをしたら何が釣れるんだろうね";
+            if (!Main.dayTime)
+                return "夜だ、君は寝ないのかい？";
+            if (Main.dayTime && Main.eclipse)
+                return "日食の日は強敵が現れる…稼ぎ時でもある";
+            return "やあ、何か買うかい？それとも売る？";
+        }
 
-		public override void AddShops() {
-			var npcShop = new NPCShop(Type, ShopName)
-				.Add(ItemID.Wood)
-				.Add(ItemID.Torch)
-				.Add(ItemID.HealingPotion)
-				.Add(ItemID.Lens)
-				.Add(ItemID.FallenStar)
-				.Add(ItemID.Gel)
+        public override void AddShops() {
+            NPCShop shop = new(Type);
+            shop.Add(ItemID.Wood)
+                .Add(ItemID.Torch)
+                .Add(ItemID.HealingPotion)
+                .Add(ItemID.Lens)
+                .Add(ItemID.FallenStar)
+                .Add(ItemID.Gel)
+                // 標準的な価格指定方法
+                .Add(new Item(ItemID.Gladius) { shopCustomPrice = Item.buyPrice(gold: 2) })
+                .Add(new Item(ItemID.Javelin) { shopCustomPrice = Item.buyPrice(copper: 2) })
+				   //.Add(Condition.Hardmode, ModContent.ItemType<AntlionRelic>())
+				   .Add<AntlionRelic>(Condition.Hardmode)
+			.Register();
+        }
 
-				// グラディウスを50銀で販売
-				.Add(ItemID.Gladius)
+        public override bool CanTownNPCSpawn(int numTownNPCs) {
+            return !NPC.AnyNPCs(NPCID.Guide);
+        }
 
-				// 投げやりを1本50銅で販売
-				.Add(ItemID.Javelin);
+        private void SummonBoss(Player player, int npcType, string bossName) {
+            if (Main.netMode == NetmodeID.MultiplayerClient) {
+                Main.npcChatText = "ボス召喚はホストのみ可能だ。";
+                return;
+            }
 
-			// Condition.HardmodeActive は、ワールドがハードモードに入った場合にのみ、このアイテムを表示します。
-			npcShop.Add<AntlionRelic>(Item.buyPrice(gold: 25)); // 例: 25ゴールドで販売
+            if (NPC.AnyNPCs(npcType)) {
+                Main.npcChatText = $"{bossName}はすでに出現している。";
+                return;
+            }
 
-			npcShop.Register();
-		}
+            // 演出：雷と光
+            for (int i = 0; i < 40; i++) {
+                Vector2 pos = player.Center + new Vector2(Main.rand.Next(-300, 300), -600);
+                Dust.NewDustPerfect(pos, DustID.Electric, Vector2.Zero, 150, Color.Cyan, 2f).noGravity = true;
+            }
 
+            SoundEngine.PlaySound(SoundID.Thunder, player.Center);
+            Main.NewText($"闘士 {bossName} が現れた！", 100, 200, 255);
 
+            // ボスを生成し、そのインスタンスを変数「spawnedBoss」に格納
+            int npcIndex = NPC.NewNPC(NPC.GetSource_FromAI(), (int)player.Center.X, (int)player.Center.Y - 400, npcType);
+            NPC spawnedBoss = Main.npc[npcIndex];
 
-		public override bool CanTownNPCSpawn(int numTownNPCs) {
-			return !NPC.AnyNPCs(NPCID.Guide);
-		}
+            // --- 強化処理 ---
+            // 1. サイズを2倍にする
+            float scaleMultiplier = 2.0f;
+            spawnedBoss.scale = scaleMultiplier;
+            spawnedBoss.width = (int)(spawnedBoss.width * scaleMultiplier);
+            spawnedBoss.height = (int)(spawnedBoss.height * scaleMultiplier);
 
-		private void SummonBoss(Player player, int npcType, string bossName) {
-			if (Main.netMode == NetmodeID.MultiplayerClient) {
-				Main.npcChatText = "ボス召喚はホストのみ可能だ。";
-				return;
-			}
+            // 2. HPを2倍にする
+            float hpMultiplier = 2.0f;
+            spawnedBoss.lifeMax = (int)(spawnedBoss.lifeMax * hpMultiplier);
+            spawnedBoss.life = spawnedBoss.lifeMax;
 
-			if (NPC.AnyNPCs(npcType)) {
-				Main.npcChatText = $"{bossName}はすでに出現している。";
-				return;
-			}
+            // 3. ダメージを半分にする
+            spawnedBoss.damage = (int)(spawnedBoss.damage * 0.5f);
 
-			//演出：雷と光
-			for (int i = 0; i < 40; i++) {
-				Vector2 pos = player.Center + new Vector2(Main.rand.Next(-300, 300), -600);
-				Dust.NewDustPerfect(pos, DustID.Electric, Vector2.Zero, 150, Color.Cyan, 2f).noGravity = true;
-			}
-
-			SoundEngine.PlaySound(SoundID.Thunder, player.Center);
-			Main.NewText($"闘士{bossName}が現れた！", 100, 200, 255);
-
-			//ここから強化処理の追加
-			// 1. サイズを2倍にする
-			float scaleMultiplier = 2.0f;
-			spawnedBoss.scale = scaleMultiplier;
-
-			// サイズが大きくなることで、接触判定の幅と高さをスケールに合わせて変更
-			spawnedBoss.width = (int)(spawnedBoss.width * scaleMultiplier);
-			spawnedBoss.height = (int)(spawnedBoss.height * scaleMultiplier);
-
-
-			// 2. HPを2倍にする
-			float hpMultiplier = 2.0f;
-			spawnedBoss.lifeMax = (int)(spawnedBoss.lifeMax * hpMultiplier);
-			spawnedBoss.life = spawnedBoss.lifeMax;
-
-			// 3. ダメージを半分にする
-			float damageMultiplier = 0.5f; 
-			spawnedBoss.damage = (int)(spawnedBoss.damage * damageMultiplier);
-
-			// 4. 防御力を+10する (既存処理)
-			int defenseBonus = 10;
-			spawnedBoss.defense += defenseBonus;
-
-			// 5. 移動速度を1.5倍にする (MaxSpeedを設定)
-			// King SlimeはAIStyle 1 (Slime) なので、MaxSpeedを調整しても直接的な効果は限定的です。
-			// しかし、ジャンプの際の最大速度はこれで制御されます。
-			float speedMultiplier = 1.5f;
-			spawnedBoss.MaxSpeed *= speedMultiplier;
-		}
-			//強化処理の追加終了
-		}
-	}
+            // 4. 防御力を+10する
+            spawnedBoss.defense += 10;
+        }
+    }
+}
